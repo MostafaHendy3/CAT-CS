6b5c5a748290572be41d439a71a69c68e8b2dfa0
MostafaHendy3 <mostafahendy9991@gmail.com>
2022-12-09 15:19:44 +0200

    SumvsXOR

---
+++ src/com/company/SumvsXOR.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class SumvsXOR {
+    public static long sumXor(long n) {
+        int counts =0;
+        String s = Long.toBinaryString(n);
+        if(n==0){
+            return (long)Math.pow(2,counts);
+        }
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                counts++;
+            }
+        }
+        return (long)Math.pow(2,counts);
+    }
+}
