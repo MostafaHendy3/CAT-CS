ba1634b1dcb866912e0b31b6659c7e51dce8e9d7
MostafaHendy3 <mostafahendy9991@gmail.com>
2022-12-09 14:59:09 +0200

    MaximizingXOR

---
+++ src/com/company/MaximizingXOR.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MaximizingXOR {
+    public static int maximizingXor(int l, int r) {
+        int max =Integer.MIN_VALUE;
+        for(int i =l;i<=r;i++){
+            for(int j = i;j<=r;j++){
+                if(max<(i^j)){
+                    max =(i^j);
+                }
+            }
+        }
+        return max;
+    }
+}
